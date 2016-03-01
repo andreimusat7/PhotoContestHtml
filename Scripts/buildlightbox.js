9f1aa5ec4cd8c8c65ee9e459fdf9cb5c7378ed7d
andreimusat7 <musatrares@gmail.com>
2016-03-01 21:46:38 +0200

    Upload project
    Upload project

---
+++ Scripts/buildlightbox.js
@@ -0,0 +1,19 @@
+﻿var lightbox = '';
+var photono = ''
+
+$(document).ready(function () {
+
+    /*Adds data to lightbox acording to frist attribute*/
+    var type = $('.open').attr('type');
+
+    if (type == 'like')
+        lightbox = "<div class='lb-dataContainer'><div class='lb-data L'><div class='lb-detailsbottom'><span class='lb-number L'></span><span class='photofull L'>Photo full size ></span></div></div><span class='photolike R'>Like</span><span class='photolikepress R'>You like this</span></div></div>";
+    if (type == 'rate') {
+        photono = "<span class='imgnolb R'>#12345</span>";
+        lightbox = "<div class='lb-dataContainer'><div class='lb-data L'><div class='lb-detailsbottom'><span class='lb-number L'></span><span class='photofull L'> Photo full size ></span></div></div><div class='imgrank R'><span class='range L'>Rank(1-100)</span><div class='rankswitch R'><span class='rankplus R'></span><input class='rankdisplay R' type='text' value='0'/><span class='rankminus R'></span></div></div></div></div>"
+    }
+    if (type == 'myphotos')
+        lightbox = "<div class='lb-dataContainer'><div class='lb-data L'><div class='lb-detailsbottom'><span class='lb-number L'></span><span class='photofull L'> Photo full size ></span></div></div><span class='photolike R'>Like</span><span class='photolikepress R'>You like this</span></div></div>";
+    if (type == 'over')
+        lightbox = "<div class='lb-dataContainer'><span class='lbtitle R'>1St place</span><div class='lb-detailsbottom L'><span class='photofull L'>Click to open full size image></span><span class='lbname R'>Name name name namen name</span></div></div></div>";
+});
---
+++ Scripts/gallery.js
@@ -0,0 +1,43 @@
+﻿
+$(document).ready(function () {
+
+    $('.imglike').click(function () {
+        $(this).hide();
+        $(this).next().show();
+    });
+
+    $('.imglikethis').click(function () {
+        $(this).hide();
+        $(this).prev().show();
+    });
+
+    $('SELECT').selectBox();
+
+    $('.photolike').on("click", function () {
+        debugger;
+        $(this).hide();
+        $(this).next().show();
+    });
+    $('.photolikepress').on("click", function () {
+        $(this).hide();
+        $(this).prev().show();
+
+    });
+
+    $('.rankminus').click(function () {
+        $value = parseInt($(this).prev().val());
+        if ($value > 0 && $value <= 100) {
+            $value = $value - 1;
+        }
+        $(this).prev().val($value);
+    });
+
+    $('.rankplus').click(function () {
+        $value = parseInt($(this).next().val());
+        if ($value >= 0 && $value < 100) {
+            $value = $value + 1;
+        }
+        $(this).next().val($value);
+    });
+
+});
